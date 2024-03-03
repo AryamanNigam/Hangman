9d5b98079a93f6d3b73ce6a2ab13dc443f6fba34
Aryaman Nigam <aryamannig@gmail.com>
2024-03-03 23:42:22 +0530

    testing of formatting

--- intro.c
+++ intro.c
@@ -9,14 +9,11 @@ int main() {
 	char randomword[1000], temp[50];  //randomword is the random word we read from that we read from the
 	char word[1000]; //word is the variable that holds the string
 	char output[50]; //output tells us whether the character is right or wrong
-	int attempts=5; //this defines the maximum number of attempts a user has
-	int counter=0, position=0, winner, length, i, match; 
 	char alphabetfromuser; //the character that the user inputs
-
 	int randomlineno, total_lines=0;
 	int current_line=0;
-	
-	
+	int attempt=5; //attempt, used for the number of times it gets the word
+	int i=0;
 	
 	
 	FILE *fp;   //line 19-40 is for reading the input
@@ -38,27 +35,40 @@ int main() {
 		}
 	}
 	fclose(fp);
-	
-	int attempt=5; //wa=wrong attempt, used for the number of times it gets the word
 
-	printf("%s\n", word);
+	printf("%s\n", word); //just to see what the word is
+		
+	char format[strlen(word)];
+	for(i=0; i<strlen(word); i++){
+		format[i]='_';
+	}
+	
+	for(i=0; i<(strlen(word)-1); i++){
+		printf("%c", format[i]);
+	}
 	
 	while (attempt != 0) {
-        printf("Enter any alphabet in lowercase\n");
+        printf("\n\n\t\tEnter any alphabet in lowercasen\n\n");
         fflush(stdout);
         scanf(" %c", &alphabetfromuser);
         getchar(); 
         int found=0;
+		int counter=0; 
 		
         for (int i = 0; i < strlen(word); i++) { 
-            if (word[i] == alphabetfromuser) {
-                printf("Yes, it's right!");
-				printf("Position: %d\n", i + 1); 
+			if (word[i] == alphabetfromuser) {
+                printf("\n\n\t\tYes, it's right!\n");
+				printf("\t\tPosition: %d\n", i + 1); 
                 found = 1;
+				format[i]=alphabetfromuser;
 				counter++;
             }
         }
         
+		for(i=0; i<(strlen(word)-1); i++){
+			printf("%c", format[i]);
+		}
+		
         if (!found) {
             printf("No, it's wrong\n");
             attempt--;
@@ -66,12 +76,12 @@ int main() {
         }
 		
 		if(counter==(strlen(word)-1)){
-			printf("Congrats, you guessed the word");
+			printf("\nCongrats, you guessed the word\n");
 			break;
 		}
 		
         if (attempt == 0) {
-            printf("Sorry, game over\n");
+            printf("\nSorry, game over\n");
             break;
         }
     }
